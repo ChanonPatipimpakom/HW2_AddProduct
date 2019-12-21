3e2d738b2f865a2551457231ce4b5eadb83199fc
ChanonPatipimpakom <chanon.white.11@gmail.com>
2019-12-21 11:40:14 +0700

    new job

--- searchproduct.php
+++ searchproduct.php
@@ -87,15 +87,57 @@
                         <input type="text" class="form-control" name="txtSearch" placeholder="Search">
                     </div>
                     <div class="col-md-2">
-                        <button class="btn btn-info btn-block"><i class="glyphicon glyphicon-search"></i> Go!
+                        <button name="submit" class="btn btn-info btn-block"><i class="glyphicon glyphicon-search"></i> Go!
                         </button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    <div class="row">
-        
+    <?php
+        if(isset($_POST['submit'])){
+            $search = $_POST['txtSearch'];
+            $sql = "SELECT * FROM product WHERE name LIKE '%$search%'";
+    ?>
+    <div class="container" style="margin-top:25px">
+        <h3>ผลการค้นหา: <?php echo $_POST['txtSearch']; ?></h3>
+        <div class="row">
+        <?php
+        $result = $conn->query($sql);
+        if(!$result){
+            echo "Error During Retrival";
+        }
+        else{
+            //ดึงข้อมูล
+            while($prd=$result->fetch_object()){
+                $prd->id; //$prd["id]
+        ?>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <a href="productdetail.php?pid=<?php echo $prd->id;?>">
+                        <img src="image/<?php echo $prd->picture;?>" alt="">
+                    </a>
+                    <div class="caption">
+                    <h3><?php echo $prd->name;?></h3>
+                    <p><?php echo $prd->description;?></p>
+                    <p><Strong>Price: </Strong><?php echo $prd->price;?><Strong> Bath</Strong></p>
+                    <p><Strong>Qty: </Strong><?php echo $prd->unitInStock;?></p>
+                    <p>
+                        <a href="#" class="btn btn-info">Add To Basket</a>
+                        <a href="editproduct.php?pid=<?php echo $prd->id?>" class="btn btn-warning"><i class="glyphicon glyphicon-pencil"></i></a>
+                        <a href="deleteproduct.php?pid=<?php echo $prd->id?>" class="btn btn-danger linkDelete"><i class="glyphicon glyphicon-trash"></i></a>
+                    </p>
+                    </div>
+                </div>
+            </div>
+            <?php
+                }
+            }
+        ?>
+        </div>
     </div>
+    <?php
+        }
+    ?>
 </body>
 </html>
