a9299db890b91fe0d81359fa740c6c58a912f7bb
ChanonPatipimpakom <chanon.white.11@gmail.com>
2020-01-25 12:05:38 +0700

    new

---
+++ insertbyajax.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    include("connect.php");
+    $name = $_POST['txtname'];
+    $desc = $_POST['txtdesc'];
+    $price = $_POST['txtprice'];
+    $unit = $_POST['txtstock'];
+    $cate = $_POST['txtcate'];
+    
+    $sql = "INSERT INTO product (name,description,price,unitInStock,picture,category) VALUES('$name', '$desc', $price, $unit, ' ', $cate)";
+
+    $result = $conn->query($sql);
+
+    $res = array();
+    if(!$result){
+        $id=-1;
+        $msg = "Error on insert".$conn->error; 
+    }
+    else{
+        $id = $conn->insert_id;
+        $msg = "Insert complete";
+    }
+    $res['id']=$id;
+    $res['msg']=$msg;
+
+    $json = json_encode($res);
+    echo $json;
+?>
--- tbproduct.php
+++ tbproduct.php
@@ -9,18 +9,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/loadingoverlay.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <div><h1>All Product</h1></div>
     <center>
-    <table width="70%" border="1">
+    <table width="70%" border="1" id="tbpro">
         <tr>
             <td>ID</td>
             <td>Name</td>
             <td>Description</td>
             <td>Price</td>
             <td>UnitStock</td>
+            <td>Category</td>
             <td>Delete</td>
         <?php
         include("connect.php");
@@ -33,13 +35,16 @@
         else {
             while($prd=$result->fetch_object()){
         ?>
-        <tr>
+        <tr id="row-<?php echo $prd->id;?>">
             <td><?php echo $prd->id;?></td>
             <td><?php echo $prd->name;?></td>
             <td><?php echo $prd->description;?></td>
             <td><?php echo $prd->price;?></td>
             <td><?php echo $prd->unitInStock;?></td>
-                <td><a href="#" class="del" data-id="<?php echo $prd->id;?>">[Del]</a></td>
+            <td><?php echo $prd->category;?></td>
+            <td>
+                <a href="#" class="del" data-id="<?php echo $prd->id;?>">[Del]</a>
+            </td>
         </tr>
         <?php
             }
@@ -48,10 +53,80 @@
         </tr>
     </table>
     </center>
+    <h3>Add Product</h3>
+    <form action="#" method="post" id="frmProduct">
+        <p>Name: <input type="text" name="txtname" id="txtname"></p>
+        <p>Description: <input type="text" name="txtdesc" id="txtdesc"></p>
+        <p>Price: <input type="text" name="txtprice" id="txtprice"></p>
+        <p>Stock: <input type="text" name="txtstock" id="txtstock"></p>
+        <p>Category: <input type="text" name="txtcate" id="txtcate"></p>
+        <p>
+            <button type="submit">OK</button>
+        </p>
+    </form>
 <script>
 $(document).ready(function(){
     $(".del").click(function(){
-        alert($(this).data("id"));
+        var pid = $(this).data("id");
+        //alert($(this).data("id"));
+        $.ajax({
+            url:"deletebyajax.php",
+            type:"get",
+            data:{ 'pid' : pid},
+            datatype:"text",
+
+            beforeSend:function(){
+                $.LoadingOverlay('show',{
+                    image:'image/clock-loading.gif',
+                    background:'rgba(200,200,200,0.6)',
+                    text:'Searching...',
+                    textResizeFactor:0.15
+                });
+            },
+
+            success:(msg)=>{
+                alert(msg);
+                //find to remove
+                var rowid ="#row-"+ pid;
+                $(rowid).remove();
+            },
+            complete:()=> $.LoadingOverlay('hide')
+        });
+    });
+    $("#frmProduct").submit(function(){
+        $.ajax({
+            url:"insertbyajax.php",
+            type:"post",
+            data: $(this).serializeArray(),
+            dataType: "json",
+            success:(res)=>{
+                if(res.id!=-1){
+                    alert(res.msg);
+                    var pname = $("#txtname").val();
+                    var desc = $("#txtdesc").val();
+                    var price = $("#txtprice").val();
+                    var stock = $("#txtstock").val();
+                    var category = $("#txtcate").val();
+                    var link =  '<a href="#" class="lnkDel" data-id="'+res.id+'">[Del]</a>';
+                    var tr = "<tr>";
+                        tr = tr + "<td>" + res.id + "</td>";
+                        tr = tr + "<td>" + pname + "</td>";
+                        tr = tr + "<td>" + desc + "</td>";
+                        tr = tr + "<td>" + price + "</td>";
+                        tr = tr + "<td>" + stock + "</td>";
+                        tr = tr + "<td>" + category + "</td>";
+                        tr = tr + "<td>" + link + "</td>";
+                    tr = tr + "</tr>";
+                    $("#tbpro").append(tr);
+                    $("#txtname").val('');
+                    $("#txtdesc").val('');
+                    $("#txtprice").val('');
+                    $("#txtstock").val('');
+                    $("#txtcate").val('');
+                }
+            }
+        });
+        return false;
     });
 });
 </script>
